848a2fc79188e902e5ca01496c01102073e393a1
Osman Cakmak <48910999+oxcakmak@users.noreply.github.com>
2023-01-14 17:08:26 +0300

    Create home.php

---
+++ app/controllers/home.php
@@ -0,0 +1,18 @@
+<?php
+class Home extends Controller{
+
+    public function index($data=[]){
+        echo $this->render->view('home', $data);
+    }
+    public function load_data(){
+
+        $model = $this->load_model('SampleModel');
+
+        $data = $model->sample_query();
+
+        $this->render->view("load_data", ["query_data" => $data]);
+
+    }
+
+}
+?>
