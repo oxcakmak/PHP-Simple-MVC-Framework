b0e4644d1d2dc836bf5f323675acd3f313484e53
Osman Cakmak <48910999+oxcakmak@users.noreply.github.com>
2023-01-21 11:09:01 +0300

    Update render.php

--- app/core/render.php
+++ app/core/render.php
@@ -1,7 +1,6 @@
 <?php
 class Render {
-    public function view($view_filename, $data = []){
-        $data = (object) $data;
+    public function view($view_filename, $data = NULL){
         if(file_exists("app/views/{$view_filename}.php")){
             include_once("app/views/{$view_filename}.php");
         }
